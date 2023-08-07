80c4679b7af1dcf36c719c0d4aff7e4a1b4574a6
Arthur <105012211+arthursousa10@users.noreply.github.com>
2023-08-07 14:34:37 -0300

    Add files via upload

---
+++ brasilCampeao.js
@@ -0,0 +1,23 @@
+const prompt = require("prompt-sync") ();
+
+console.log("Desafio Copa do Mundo - Ano em que o Brasil foi campeão");
+console.log("======================================================");
+
+const anosCampeao = [1958, 1962, 1970, 1994, 2002];
+let tentativas = 0;
+let acertos = 0;
+
+while (acertos < 5) {
+    const pergunta = `Em que ano o Brasil foi campeão da Copa do Mundo? (${acertos + 1}/5)`;
+    const respostaUsuario = Number(prompt(pergunta));
+    tentativas++;
+
+    if (anosCampeao.includes(respostaUsuario)) {
+        console.log("Parabéns! Resposta correta!");
+        acertos++;
+    } else {
+        console.log("Resposta incorreta. Tente novamente!");
+    }
+}
+
+console.log(`Parabéns! Você acertou todas as 5 perguntas em ${tentativas} tentativas.`);
